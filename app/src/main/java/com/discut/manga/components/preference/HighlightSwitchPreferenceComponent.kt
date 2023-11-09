d3d22f2f6d32eff010801d28284f21859f7e546f
Discut <siiiro@outlook.com>
2023-11-09 11:16:26 +0800

    refactor(HighlightSwitch): 重构模块HSPC

--- app/src/main/java/com/discut/manga/components/preference/HighlightSwitchPreferenceComponent.kt
+++ app/src/main/java/com/discut/manga/components/preference/HighlightSwitchPreferenceComponent.kt
@@ -1,28 +1,37 @@
 package com.discut.manga.components.preference
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.DividerDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.discut.manga.theme.MangaTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HighlightSwitchPreferenceComponent(
     modifier: Modifier = Modifier,
@@ -30,25 +39,42 @@ fun HighlightSwitchPreferenceComponent(
     onCheckedChange: ((Boolean) -> Unit)? = null,
     title: String
 ) {
-    BasePreferenceComponent(
+    Surface(
         modifier = modifier
-            .padding(horizontal = 16.dp)
-            .background(
-                color = MaterialTheme.colorScheme.primaryContainer,
-                shape = MaterialTheme.shapes.extraLarge
-            )
-            .padding(vertical = 8.dp)
-            .padding(start = 6.dp),
-        title = title,
-        titleFontSize = LabelFontSize,
-        endWidget = {
-            Switch(
-                checked = checked,
-                onCheckedChange = null
-            )
-        },
-        onClick = { onCheckedChange?.invoke(!checked) }
-    )
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .heightIn(min = 68.dp)
+            .fillMaxWidth(),
+        color = MaterialTheme.colorScheme.primaryContainer,
+        shape = MaterialTheme.shapes.extraLarge,
+        onClick = { onCheckedChange?.invoke(!checked) },
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(vertical = 16.dp)
+            ) {
+                if (title.isNotBlank()) {
+                    Text(
+                        text = title,
+                        modifier = Modifier.padding(horizontal = 16.dp),
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 1,
+                        style = MaterialTheme.typography.titleLarge,
+                        fontSize = LabelFontSize
+                    )
+                }
+            }
+            Box(modifier = Modifier.padding(end = 16.dp)) {
+                Switch(
+                    checked = checked,
+                    onCheckedChange = null
+                )
+            }
+        }
+    }
 }
 
 
@@ -82,7 +108,7 @@ private fun HighlightSwitchPreferenceComponentPreview() {
         mutableStateOf(false)
     }
     MangaTheme {
-        Box() {
+        Box {
             HighlightSwitchPreferenceComponent(
                 modifier = Modifier,
                 title = "开启“自动适应亮度”",
