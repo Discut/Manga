2e1af274abdd255f3b9757feb1f956c754a4e0c5
Discut <siiiro@outlook.com>
2023-11-24 15:52:12 +0800

    feat(App): 更新依赖为toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,18 +46,22 @@ android {
     }
 
 }
-
 dependencies {
-    implementation(project(mapOf("path" to ":data")))
-    val room_version = "2.5.1"
-
+    val roomVersion = "2.5.1"
     val composeBom = platform("androidx.compose:compose-bom:2023.01.00")
+
+    implementation(project(mapOf("path" to ":data")))
+    implementation(project(mapOf("path" to ":source-local")))
+    implementation(project(mapOf("path" to ":core")))
+    implementation(project(mapOf("path" to ":extension")))
+    implementation(project(mapOf("path" to ":source-api")))
+
+    // Base
     implementation(composeBom)
     androidTestImplementation(composeBom)
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation(libs.material)
 
     // Material Design 3
     implementation("androidx.compose.material3:material3")
@@ -66,75 +70,65 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
 
-    // UI Tests
+    // UI
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-
-    // Optional - Included automatically by material, only add when you need
-    // the icons but not the material library (e.g. when using Material3 or a
-    // custom design system based on Foundation)
-    implementation("androidx.compose.material:material-icons-core")
-    // Optional - Add full set of material icons
-    implementation("androidx.compose.material:material-icons-extended")
-    // Optional - Add window size utils
-    implementation("androidx.compose.material3:material3-window-size-class")
-
-    // Optional - Integration with activities
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.compose.material:material-icons-core")// MD icon
+    implementation("androidx.compose.material:material-icons-extended")// Add full set of material icons
+    implementation("androidx.compose.material3:material3-window-size-class")// Add window size utils
+    implementation("androidx.activity:activity-compose:1.7.2")// Integration with activities
+    implementation(libs.constraintlayout)
 
     // for unlock app
-    implementation("androidx.biometric:biometric-ktx:1.2.0-alpha05")
+    implementation(libs.biometric.ktx)
 
     // for DI
-    implementation("com.google.dagger:hilt-android:2.48.1")
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
 
     // lifecycle
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.lifecycle.runtime.compose)
+    implementation(libs.lifecycle.viewmodel.compose)
 
     // navigation
-    implementation("androidx.navigation:navigation-compose:2.7.5")
+    implementation(libs.navigation.compose)
 
     // for datastore
-    implementation("androidx.datastore:datastore:1.0.0")
+    implementation(libs.datastore)
 
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation(project(mapOf("path" to ":core")))
-    implementation(project(mapOf("path" to ":extension")))
-    implementation(project(mapOf("path" to ":source-api")))
-    kapt("com.google.dagger:hilt-android-compiler:2.48.1")
 
-    //ui lib
-    implementation("com.github.tachiyomiorg:DirectionalViewPager:1.0.0") {
+    //Ui Lib
+    implementation(libs.directionalviewpager) {
         exclude(group = "androidx.viewpager", module = "viewpager")
     }
 
-    // image loading
-    implementation("com.github.tachiyomiorg:subsampling-scale-image-view:c8e2650")/*{
+    // Image Loading
+    implementation(libs.subsamplingscaleimageview)/*{
         exclude(module = "image-decoder")
     }
     implementation("com.github.tachiyomiorg:image-decoder:16eda64574")*/
 
-    // for room
-
-/*    // To use Kotlin annotation processing tool (kapt)
-    kapt("androidx.room:room-compiler:$room_version")*/
+    // Room
+    // To use Kotlin annotation processing tool (kapt)
+    /*kapt("androidx.room:room-compiler:$room_version")*/
     // To use Kotlin Symbol Processing (KSP)
-    //noinspection GradleDependency
-    ksp("androidx.room:room-compiler:$room_version")
+    ksp(libs.room.compiler)
+    implementation(libs.room.ktx)// Kotlin Extensions and Coroutines support for Room
+    implementation(libs.room.paging)// Paging 3 Integration
+    testImplementation(libs.room.test)// Test helpers
 
-    // optional - Kotlin Extensions and Coroutines support for Room
-    implementation("androidx.room:room-ktx:$room_version")
+    //// Network
+    // okhttp
+    implementation(libs.bundles.okhttp)
 
-    // optional - Test helpers
-    testImplementation("androidx.room:room-testing:$room_version")
+    //// Disk
+    // cache
+    implementation(libs.disklrucache)
+    // stream function
+    implementation(libs.okio)
 
-    // optional - Paging 3 Integration
-    implementation("androidx.room:room-paging:$room_version")
-
-    // end room
-
+    //// Test
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
