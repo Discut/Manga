44315086c08fa1910ad6f06506eb27c3e798532e
Discut <siiiro@outlook.com>
2023-12-05 18:23:58 +0800

    feat(Component): CustomModalBottomSheet.kt

---
+++ app/src/main/java/com/discut/manga/components/CustomModalBottomSheet.kt
@@ -0,0 +1,48 @@
+package com.discut.manga.components
+
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.captionBar
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import com.discut.manga.components.utils.maxHeightWithoutStatusBar
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomModalBottomSheet(
+    modifier: Modifier = Modifier,
+    isShow: Boolean = false,
+    sheetState: SheetState = rememberModalBottomSheetState(),
+    sheetMaxWidth: Dp = BottomSheetDefaults.SheetMaxWidth,
+
+    onDismissRequest: () -> Unit = {},
+    content: @Composable ColumnScope.(PaddingValues) -> Unit
+) {
+    if (isShow) {
+        val bottomPadding =
+            WindowInsets.navigationBars.asPaddingValues()
+        ModalBottomSheet(
+            modifier = Modifier
+                //.requiredHeightIn(max = getMaxHeightWithoutStatusBar())
+                .maxHeightWithoutStatusBar()
+                .then(modifier),
+            sheetState = sheetState,
+            sheetMaxWidth = sheetMaxWidth,
+            windowInsets = WindowInsets.captionBar,
+            onDismissRequest = onDismissRequest,
+        ) {
+            content(
+                bottomPadding
+            )
+        }
+    }
+}
