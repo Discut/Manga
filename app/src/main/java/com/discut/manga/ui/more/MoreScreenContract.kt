32cf0170c4a7dd157988b0844cd4dd18fec7d746
Discut <siiiro@outlook.com>
2023-12-13 17:34:13 +0800

    fix(MoreScreen): 修复More页面隐私模式开关状态不能同步的问题

--- app/src/main/java/com/discut/manga/ui/more/MoreScreenContract.kt
+++ app/src/main/java/com/discut/manga/ui/more/MoreScreenContract.kt
@@ -3,13 +3,13 @@ package com.discut.manga.ui.more
 import com.discut.core.mvi.contract.UiEffect
 import com.discut.core.mvi.contract.UiEvent
 import com.discut.core.mvi.contract.UiState
-import com.discut.manga.ui.main.domain.NavBarItem
 
 data class MoreScreenState(val enableSecurityMode: Boolean = false) : UiState
 
 sealed interface MoreScreenEvent : UiEvent {
 
-    data class ClickSecurityMode(val enable: Boolean) : MoreScreenEvent
+    data class SecurityModeChanged(val enable: Boolean) : MoreScreenEvent
+
 }
 
 sealed interface MoreScreenEffect : UiEffect {
--- app/src/main/java/com/discut/manga/ui/more/MoreScreenViewModel.kt
+++ app/src/main/java/com/discut/manga/ui/more/MoreScreenViewModel.kt
@@ -1,8 +1,12 @@
 package com.discut.manga.ui.more
 
+import androidx.lifecycle.viewModelScope
 import com.discut.core.mvi.BaseViewModel
 import com.discut.manga.util.get
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import manga.core.preference.PreferenceManager
 import manga.core.preference.SettingsPreference
 import javax.inject.Inject
@@ -12,6 +16,16 @@ class MoreScreenViewModel @Inject constructor() :
     BaseViewModel<MoreScreenState, MoreScreenEvent, MoreScreenEffect>() {
 
     private val settingsPreference = PreferenceManager.get<SettingsPreference>()
+
+    init {
+        settingsPreference
+            .getSecurityModeAsFlow()
+            .distinctUntilChanged()
+            .onEach {
+                sendEvent(MoreScreenEvent.SecurityModeChanged(it))
+            }
+            .launchIn(viewModelScope)
+    }
 
     override fun initialState(): MoreScreenState {
         return MoreScreenState(
@@ -24,7 +38,7 @@ class MoreScreenViewModel @Inject constructor() :
         state: MoreScreenState
     ): MoreScreenState {
         return when (event) {
-            is MoreScreenEvent.ClickSecurityMode -> {
+            is MoreScreenEvent.SecurityModeChanged -> {
                 settingsPreference.setSecurityMode(event.enable)
                 sendEffect(MoreScreenEffect.SecurityModeChange(event.enable))
                 state.copy(
