d61f6a586e6df9908e9f9b4695ee88e4931a2f59
Discut <siiiro@outlook.com>
2024-01-11 09:43:57 +0800

    feat: DownloadPreference

--- core/src/main/java/manga/core/preference/DownloadPreference.kt
+++ core/src/main/java/manga/core/preference/DownloadPreference.kt
@@ -1,10 +1,49 @@
 package manga.core.preference
 
+import android.content.Context
 import android.content.SharedPreferences
+import discut.manga.common.res.R
 import kotlinx.coroutines.flow.Flow
+import manga.core.disk.DiskUtils
+import manga.core.utils.getStringFromLocal
+import java.io.File
+import java.util.Locale
+
 
 class DownloadPreference constructor(appPreference: SharedPreferences, flow: Flow<String?>) :
     AppPreference(appPreference, flow) {
+    companion object {
+        const val DOWNLOAD_DIRECTORY = "storage/emulated/0/Download/Manga"
+    }
+
+    fun getDownloadDirectory(context: Context): String {
+        if (isExists("download_directory").not()) {
+            val baseLocationFolder =
+                "${
+                    context.getStringFromLocal(
+                        R.string.app_name,
+                        Locale.ENGLISH
+                    )
+                }${File.separator}download"
+            val first =
+                DiskUtils.getExternalStorages(context)
+                    .map { File(it.absolutePath, baseLocationFolder) }
+                    .first()
+            edit {
+                putString("download_directory", first.absolutePath)
+            }
+        }
+        return getValue(
+            "download_directory",
+            context.externalCacheDir?.absolutePath ?: DOWNLOAD_DIRECTORY
+        )
+    }
+
+    fun setDownloadDirectory(path: String) {
+        edit {
+            putString("download_directory", path)
+        }
+    }
 
     fun isWifiOnly(): Boolean =
         getValue("enable_wifi_only", false)
